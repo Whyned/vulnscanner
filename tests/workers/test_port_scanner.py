be3ea048f8e384d679da39c154e7e6a8fe174077
whyned <whyned@mail.ru>
2018-02-02 17:22:03 +0100

    added tests for port_scanner

--- tests/workers/test_port_scanner.py
+++ tests/workers/test_port_scanner.py
@@ -1,7 +1,45 @@
+import socket
+
 from vulnscanner.workers.port_scanner import PortScannerWorker
 from vulnscanner import logger
 logger.attach()
 
-def test_PortScannerWorker():
+original_logger_result = logger.result
+
+def teardown_module():
+    global logger
+    logger.result = original_logger_result
+
+
+def test_PortScannerWorker_1():
+    host = '127.0.0.1'
+    port = 9999
+    test_PortScannerWorker_1.fetched = False
+    def result_fetcher(worker_name, type, file, str):
+        test_PortScannerWorker_1.fetched = True
+        assert type == 'open'
+        assert str == '%s:%s' %(host,port)
+
+    logger.result = result_fetcher
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen(1)
+
     pw = PortScannerWorker({'timeout': 10})
-    pw.processHostPort('google.de', 80)
+    pw.processHostPort(host, port)
+
+    s.close()
+
+    assert test_PortScannerWorker_1.fetched is True
+
+def test_PortScannerWorker_2():
+    test_PortScannerWorker_2.fetched = False
+    def result_fetcher(worker_name, type, file, str):
+        test_PortScannerWorker_2.fetched = True
+
+    logger.result = result_fetcher
+
+    pw = PortScannerWorker({'timeout': 10})
+    pw.processHostPort('127.0.0.1', 9999)
+
+    assert test_PortScannerWorker_2.fetched is False
